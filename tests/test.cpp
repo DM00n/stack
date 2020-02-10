b4c205ff896ab741998b25d2d892a966a9dfc8ce
Дмитрий Астахов <teamvortex@yandex.ru>
2020-02-10 15:20:34 +0300

    v0.2

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,6 +4,37 @@
 #include <header1.hpp>
 #include <header2.hpp>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+class for_test2{
+public:
+    for_test(double a, double b, double c){
+        square = a * b;
+        volume = a * b * c;
+    }
+    for_test(double a, double b){
+        square = a * b;
+        volume = 0;
+    }
+    double square;
+    double volume;
+};
+
+TEST(task1, EmptyTest) {
+unsigned a = 516;
+
+stack<unsigned> mystk;
+mystk.push(a);
+mystk.push(2314);
+
+EXPECT_EQ(mystk.head(), 2314);
+mystk.pop();
+EXPECT_EQ(mystk.head(), 516);
+}
+
+TEST(task1, EmptyTest) {
+stack<for_test2> mytest;
+mytest.push(for_test2(3, 4));
+mytest.push_emplace(3, 4, 7);
+EXPECT_EQ(mytest.head().volume, 84);
+mytest.pop();
+EXPECT_EQ(mytest.head().square, 12);
 }
